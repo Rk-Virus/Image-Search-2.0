a805d418221d94e1b34b1b6080f695cddcf725d5
Ravi Mishra <ravim70655@gmail.com>
2025-11-09 00:53:01 +0530

    Update Login.jsx

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 export default function Login() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -21,7 +23,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5001/api/auth/login', formData, {
+      const res = await axios.post(API_BASE_URL+'/api/auth/login', formData, {
         withCredentials: true,
       });
       
@@ -38,17 +40,17 @@ export default function Login() {
   const handleGoogleLogin = (e) => {
     e.preventDefault();
     // Directly redirect to the server's Google auth endpoint
-    window.location.href = 'http://localhost:5001/api/auth/google';
+    window.location.href = API_BASE_URL+'/api/auth/google';
   };
 
   const handleGithubLogin = (e) => {
     e.preventDefault();
-    window.location.href = 'http://localhost:5001/api/auth/github';
+    window.location.href = API_BASE_URL+'/api/auth/github';
   };
 
   const handleFacebookLogin = (e) => {
     e.preventDefault();
-    window.location.href = 'http://localhost:5001/api/auth/facebook';
+    window.location.href = API_BASE_URL+'/api/auth/facebook';
   };
 
   return (
