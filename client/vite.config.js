cda6d5994be4ab9accf36ddecfa672df132a6302
rk-virus <ravim70655@gmail.com>
2026-01-09 18:01:36 +0530

    added pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugIn = {
+  registerType:'autoUpdate',
+  includeAssests:['favicon.ico', "apple-touch-icon.png", "maskable-icon.png"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'any'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'any',
+    },
+    {
+      src: '/maskable-icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait',
+  },
+  // devOptions: {
+  //   enabled: true,   // 👈 enables PWA in dev
+  // },
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugIn)],
 })
